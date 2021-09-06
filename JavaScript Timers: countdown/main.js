b66504fa715a8c9c661097d764c9c3b40d479656
Ryan Kim <ryankrdh@gmail.com>
2021-09-06 12:08:54 -0700

    Planning the code for random game

--- JavaScript Timers: countdown/main.js
+++ JavaScript Timers: countdown/main.js
@@ -3,7 +3,7 @@
 /* Timers Exercise
 Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds decrements the value and console.logs it. Once the value is 0 it should load "DONE!" and stop.
 
-countDwon(4);
+countDown(4);
 // 3
 // 2
 // 1
@@ -13,7 +13,13 @@ countDwon(4);
 function countDown(num) {
   let timerID = setInterval(function () {
     num--;
-    console.log(num);
+    // in order for the countdown to wait to show DONE as part of the countdown, console.log needs to be after the if statement.
+    if (num == 0) {
+      clearInterval(timerID);
+      console.log('DONE!');
+    } else {
+      console.log(num);
+    }
   }, 1000);
 }
 
