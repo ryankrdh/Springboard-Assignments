bdaa3fb0c238c39b8d7eef3cffc6f967f10890f6
Ryan Kim <ryankrdh@gmail.com>
2021-09-06 10:25:54 -0700

    starting countdown exercise

--- JavaScript Timers: countdown/main.js
+++ JavaScript Timers: countdown/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
 /* Timers Exercise
-Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds decrements the value and console.logs it. Once the value is 0 it should lod "DONT!" and stop.
+Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds decrements the value and console.logs it. Once the value is 0 it should load "DONE!" and stop.
+
+countDwon(4);
+// 3
+// 2
+// 1
+// "DONE!"
 */
+
+function countDown(num) {}
+let timerID = setInterval(function () {
+  time--;
+}, 1000);
+
+countDown(4);
