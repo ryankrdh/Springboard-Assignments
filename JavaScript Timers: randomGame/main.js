34538f3a8b2be58b6f063970bf393784285e9a11
Ryan Kim <ryankrdh@gmail.com>
2021-09-06 12:39:26 -0700

    initial codes completed.

--- JavaScript Timers: randomGame/main.js
+++ JavaScript Timers: randomGame/main.js
@@ -4,10 +4,19 @@
 Write a function called random game that selects a random number between 0 and 1 every 1000 miliseconds and each time that a random number is picked, add 1 counter. If the number is greater than .75, stop the timer and console.log the number of tries it took before we found anumber greater than .75.
 */
 
-// randomize number 0 and 1 every 1000 milliseconds.
-
-// add 1 to a counter every time a random number is picked.
-
-// If the picked number is greater than .75, stop the timer.
-
-// stop the timer, console.log the number of tries.
+function randomGame() {
+    let counter = 0;
+    let randomNumber = 0;
+    // randomize number 0 and 1 every 1000 milliseconds.
+    // let randomNumber = Math.random();
+    let gameGenerator = setInterval(function () {
+        randomNumber = Math.random();
+        counter ++;
+        // If the picked number is greater than .75, stop the timer.
+        if (randomNumber > .75) {
+            // stop the timer, console.log the number of tries.
+            console.log(counter);
+            break;
+        }
+    }, 1000)
+}
