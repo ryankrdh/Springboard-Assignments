19f9382958a48f6a3dfe1e3848bcf9ebc4003f7f
Ryan Kim <ryankrdh@gmail.com>
2021-09-09 20:07:17 -0700

    DOM manipulation assignment

--- DOM Manipulation Exercises/app.js
+++ DOM Manipulation Exercises/app.js
@@ -1,24 +1,41 @@
 'use strict';
 
+/*
 // Select the section with an id of container without using querySelector.
-
 const section = document.getElementById('container');
-// console.log(section);
+
 
 // Select the section with an id of container using querySelector.
+const section = document.querySelector('#container');
 
-const section = document.querySelector('container');
-console.log(section);
+
 // Select all of the list items with a class of “second”.
+const lists = document.querySelectorAll('.second');
+
 
 // Select a list item with a class of third, but only the list item inside of the ol tag.
+const lists = document.querySelectorAll('ol .third');
+
 
 // Give the section with an id of container the text “Hello!”.
+let section = document.querySelector('#container');
+section.innterText = 'Hello!';
+// console.log(section);
+
 
 // Add the class main to the div with a class of footer.
+let div = document.querySelector('.footer');
+div.classList.add('main');
+// console.log(div);
+
 
 // Remove the class main on the div with a class of footer.
+let div = document.querySelector('.footer');
+div.classList.remove('main');
+// console.log(div);
 
+
+*/
 // Create a new li element.
 
 // Give the li the text “four”.
