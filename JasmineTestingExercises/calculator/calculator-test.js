f9bb02e5b48950a8129c01c4b5ac55137fa20c57
Ryan Kim <ryankrdh@gmail.com>
2021-09-24 21:29:09 -0700

    working on calculate monthly payment function

--- JasmineTestingExercises/calculator/calculator-test.js
+++ JasmineTestingExercises/calculator/calculator-test.js
@@ -1,10 +1,8 @@
-
 it('should calculate the monthly rate correctly', function () {
   // ...
 });
 
-
-it("should return a result with 2 decimal places", function() {
+it('should return a result with 2 decimal places', function () {
   // ..
 });
 
--- JasmineTestingExercises/calculator/calculator.js
+++ JasmineTestingExercises/calculator/calculator.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById("calc-form");
+window.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('calc-form');
   if (form) {
     setupIntialValues();
-    form.addEventListener("submit", function(e) {
+    form.addEventListener('submit', function (e) {
       e.preventDefault();
       update();
     });
@@ -11,30 +11,44 @@ window.addEventListener('DOMContentLoaded', function() {
 
 function getCurrentUIValues() {
   return {
-    amount: +(document.getElementById("loan-amount").value),
-    years: +(document.getElementById("loan-years").value),
-    rate: +(document.getElementById("loan-rate").value),
-  }
+    amount: +document.getElementById('loan-amount').value,
+    years: +document.getElementById('loan-years').value,
+    rate: +document.getElementById('loan-rate').value,
+  };
 }
 
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
+
+// The following will have the devault values unless updated by the user
 function setupIntialValues() {
+  const defaultValues = { amount: 300000, years: 30, rate: 3 };
+  const amountUI = document.querySelector('#loan-amount');
+  amountUI.value = defaultValues.amount;
+  console.log(amountUI);
+  const yearsUI = document.querySelector('#loan-years');
+  yearsUI.value = defaultValues.years;
+  const rateUI = document.querySelector('#loan-rate');
+  rateUI.value = defaultValues.rate;
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const updateValue = getCurrentUIValues();
+  return;
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  // Amount of monthly rate
+  let monthlyRate = rate / 100 / 12;
+  let numberOfMonths = years * 12;
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
-function updateMonthly(monthly) {
-}
+function updateMonthly(monthly) {}
