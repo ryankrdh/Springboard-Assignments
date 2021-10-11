37503199273155f07a4dd006f623b7a324bd0edd
Ryan Kim <ryankrdh@gmail.com>
2021-10-10 20:25:28 -0700

    testing 1st arrow function exercise

--- Arrow-Function-Exercise/exercise.js
+++ Arrow-Function-Exercise/exercise.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // ARROW FUNCTION EXERCISE
+/*
+function double(arr) {
+  return arr.map(function (val) {
+    return val * 2;
+  });
+}
+*/
+
+function double(arr) 
+    arr.map((val) => val * 2);
