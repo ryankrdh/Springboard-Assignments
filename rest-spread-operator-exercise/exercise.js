53c0463a5f1a4e1dc21784f89a6dddcbe6bd9904
Ryan Kim <ryankrdh@gmail.com>
2021-10-13 22:26:14 -0700

    rest spread operator exercise 2

--- rest-spread-operator-exercise/exercise.js
+++ rest-spread-operator-exercise/exercise.js
@@ -9,3 +9,11 @@ function filterOutOdds() {
 }
 
 // Refactor
+const filterOutOdds = (...arguments) => arguments.filter(value => value % 2 === 0)
+
+// 2. findMin: Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
+// Make sure to do this using the rest and spread operator.
+
+findMin(1,4,12,-3) // -3
+findMin(1,-1) // -1
+findMin(3,1) // 1
