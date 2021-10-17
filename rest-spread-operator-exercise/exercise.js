101f46230f0c8daf3da764df1dae351d5fc6604c
Ryan Kim <ryankrdh@gmail.com>
2021-10-16 21:23:10 -0700

    rest spread operator exercise 4

--- rest-spread-operator-exercise/exercise.js
+++ rest-spread-operator-exercise/exercise.js
@@ -27,5 +27,12 @@ findMin(3,1) // 1
 // 3. mergeObjects
 // Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
 
-const mergeObjects = (obj1, obj2) => (obj1 + obj2)
+const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2})
 mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
+
+// 4. doubleAndReturnArgs
+// Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
+
+const doubleAndReturnArgs = 
+doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
+doubleAndReturnArgs([2],10,4) // [2, 20, 8]
