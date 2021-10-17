7dfc68a82b4f570dfbe97dd36e5f2ae73ecd6aa0
Ryan Kim <ryankrdh@gmail.com>
2021-10-17 14:52:58 -0700

    rest spread operator exercise 5

--- rest-spread-operator-exercise/exercise.js
+++ rest-spread-operator-exercise/exercise.js
@@ -1,83 +1,87 @@
 'use strict';
 
-// 1. Refactor the following function:
-function filterOutOdds() {
-  var nums = Array.prototype.slice.call(arguments);
-  return nums.filter(function (num) {
-    return num % 2 === 0;
-  });
-}
+// // 1. Refactor the following function:
+// function filterOutOdds() {
+//   var nums = Array.prototype.slice.call(arguments);
+//   return nums.filter(function (num) {
+//     return num % 2 === 0;
+//   });
+// }
 
-// Refactor
-const filterOutOdds = (...arguments) => arguments.filter((value) => value % 2 === 0)
+// // Refactor
+// const filterOutOdds = (...arguments) => arguments.filter((value) => value % 2 === 0)
 
-// 2. findMin: Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
-// Make sure to do this using the rest and spread operator.
+// // 2. findMin: Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
+// // Make sure to do this using the rest and spread operator.
 
-// const findMin = (...args).reduce((min, currNum) => {
-//     return Math.min(min, currNum);
-// });
+// // const findMin = (...args).reduce((min, currNum) => {
+// //     return Math.min(min, currNum);
+// // });
 
-const findMin = (...args) => Math.min(...args)
+// const findMin = (...args) => Math.min(...args)
 
-findMin(1,4,12,-3) // -3
-findMin(1,-1) // -1
-findMin(3,1) // 1
+// findMin(1,4,12,-3) // -3
+// findMin(1,-1) // -1
+// findMin(3,1) // 1
 
-// 3. mergeObjects
-// Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+// // 3. mergeObjects
+// // Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
 
-const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2})
-mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
+// const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2})
+// mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
 
-// 4. doubleAndReturnArgs
-// Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
+// // 4. doubleAndReturnArgs
+// // Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
 
-const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(value => value * 2)]
-doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
-doubleAndReturnArgs([2],10,4) // [2, 20, 8]
+// const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(value => value * 2)]
+// doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
+// doubleAndReturnArgs([2],10,4) // [2, 20, 8]
 
-// 5. Slice and Dice!
-// For this section, write the following functions using rest, spread and refactor these functions to be arrow functions! Make sure that you are always returning a new array or object and not modifying the existing inputs.
+// // 5. Slice and Dice!
+// // For this section, write the following functions using rest, spread and refactor these functions to be arrow functions! Make sure that you are always returning a new array or object and not modifying the existing inputs.
 
-/** remove a random element in the items array
-and return a new array without that item. */
+// /** remove a random element in the items array
+// and return a new array without that item. */
 
-const removeRandom = items => {
-    let randomElement = Math.floor(Math.random() * (max(items.length) - min(items.length) + 1) + min(items.length))
-}
+const items = ['a', 'b', 'c', 'd', 'e', 'f'];
 
+const removeRandom = (items) => {
+  let randomElement = Math.floor(
+    Math.random() * (Math.max(items.length) - Math.min(items.length) + 1) +
+      Math.min(items.length)
+  );
+  return randomElement;
+};
 
-/** Return a new array with every item in array1 and array2. */
+removeRandom(items);
 
-function extend(array1, array2) {
+// /** Return a new array with every item in array1 and array2. */
 
-}
+// function extend(array1, array2) {
 
-/** Return a new object with all the keys and values
-from obj and a new key/value pair */
+// }
 
-function addKeyVal(obj, key, val) {
+// /** Return a new object with all the keys and values
+// from obj and a new key/value pair */
 
-}
+// function addKeyVal(obj, key, val) {
 
+// }
 
-/** Return a new object with a key removed. */
+// /** Return a new object with a key removed. */
 
-function removeKey(obj, key) {
+// function removeKey(obj, key) {
 
-}
+// }
 
+// /** Combine two objects and return a new object. */
 
-/** Combine two objects and return a new object. */
+// function combine(obj1, obj2) {
 
-function combine(obj1, obj2) {
+// }
 
-}
+// /** Return a new object with a modified key and value. */
 
+// function update(obj, key, val) {
 
-/** Return a new object with a modified key and value. */
-
-function update(obj, key, val) {
-
-}
+// }
