4b60b5137c039e9501c396fd944914f83dde8a42
Ryan Kim <ryankrdh@gmail.com>
2021-10-17 14:37:32 -0700

    rest spread operator exercise 5

--- rest-spread-operator-exercise/exercise.js
+++ rest-spread-operator-exercise/exercise.js
@@ -39,3 +39,45 @@ doubleAndReturnArgs([2],10,4) // [2, 20, 8]
 
 // 5. Slice and Dice!
 // For this section, write the following functions using rest, spread and refactor these functions to be arrow functions! Make sure that you are always returning a new array or object and not modifying the existing inputs.
+
+/** remove a random element in the items array
+and return a new array without that item. */
+
+const removeRandom = items => {
+    let randomElement = Math.floor(Math.random() * (max(items.length) - min(items.length) + 1) + min(items.length))
+}
+
+
+/** Return a new array with every item in array1 and array2. */
+
+function extend(array1, array2) {
+
+}
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+function addKeyVal(obj, key, val) {
+
+}
+
+
+/** Return a new object with a key removed. */
+
+function removeKey(obj, key) {
+
+}
+
+
+/** Combine two objects and return a new object. */
+
+function combine(obj1, obj2) {
+
+}
+
+
+/** Return a new object with a modified key and value. */
+
+function update(obj, key, val) {
+
+}
