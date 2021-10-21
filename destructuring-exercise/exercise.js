23fa41e85786f0c57f69cece958dcfa86b64ce68
Ryan Kim <ryankrdh@gmail.com>
2021-10-20 22:00:20 -0700

    working on destructuring exercise

--- destructuring-exercise/exercise.js
+++ destructuring-exercise/exercise.js
@@ -6,3 +6,109 @@ let { numPlanets, yearNeptuneDiscovered } = facts;
 
 console.log(numPlanets); // 8
 console.log(yearNeptuneDiscovered); // 1846
+
+// --------------------------------------------------------------------
+
+// Object Destrring 2
+// What does the following code return/print?
+
+let planetFacts = {
+  numPlanets: 8,
+  yearNeptuneDiscovered: 1846,
+  yearMarsDiscovered: 1659,
+};
+
+let { numPlanets, ...discoveryYears } = planetFacts;
+
+console.log(discoveryYears); // yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659
+
+// --------------------------------------------------------------------
+
+// Object Destructuring 3
+// What does the following code return/print?
+
+function getUserData({ firstName, favoriteColor = 'green' }) {
+  return `Your name is ${firstName} and you like ${favoriteColor}`;
+}
+
+getUserData({ firstName: 'Alejandro', favoriteColor: 'purple' }); // Your name is Alejandro and you like purple.
+getUserData({ firstName: 'Melissa' }); // Your name is Melissa and you like green.
+getUserData({}); // Your name is undefined and you like green.
+
+// --------------------------------------------------------------------
+
+// Array Destructuring 1
+// What does the following code return/print?
+
+let [first, second, third] = ['Maya', 'Marisa', 'Chi'];
+
+console.log(first); // Maya
+console.log(second); // Marisa
+console.log(third); // Chi
+
+// --------------------------------------------------------------------
+
+// Array Destructuring 2
+// What does the following code return/print?
+
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+  'Raindrops on roses',
+  'whiskers on kittens',
+  'Bright copper kettles',
+  'warm woolen mittens',
+  'Brown paper packages tied up with strings',
+];
+
+console.log(raindrops); // Raindrops on roses
+console.log(whiskers); // whiskers on kittens
+console.log(aFewOfMyFavoriteThings); // ['Bright copper kettles', 'warm woolen mittens', 'Brown paper packages tied up with strings']
+
+// --------------------------------------------------------------------
+
+// Array Destructuring 3
+// What does the following code return/print?
+
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]];
+
+console.log(numbers); // [10, 30, 20]
+
+// --------------------------------------------------------------------
+
+// ES5 Assigning Variables to Object Properties
+var obj = {
+  numbers: {
+    a: 1,
+    b: 2,
+  },
+};
+
+var a = obj.numbers.a;
+var b = obj.numbers.b;
+
+// ES2015 One-Line Array Swap with Destructuring
+/* Write an ES2015 Version */
+
+const obj = {
+  numbers: {
+    a: 1,
+    b: 2,
+  },
+};
+
+//option 1
+const {
+  numbers: { a, b },
+} = obj;
+
+// option 2
+const { a, b } = obj.numbers;
+// --------------------------------------------------------------------
+
+// --------------------------------------------------------------------
+
+// --------------------------------------------------------------------
+
+// --------------------------------------------------------------------
+
+// --------------------------------------------------------------------
