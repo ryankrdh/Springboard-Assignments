7346914794939745710ee8cdd6996ba044fcd089
Ryan Kim <ryankrdh@gmail.com>
2021-10-21 22:33:38 -0700

    working on destructuring exercise

--- destructuring-exercise/exercise.js
+++ destructuring-exercise/exercise.js
@@ -121,6 +121,29 @@ console.log(arr);
 
 // --------------------------------------------------------------------
 
+// raceResults()
+// Write a function called raceResults which accepts a single array argument. It should return an object with the keys first, second, third, and rest.
+
+// first: the first element in the array
+// second: the second element in the array
+// third: the third element in the array
+// rest: all other elements in the array
+// Write a one line function to make this work using
+// An arrow function
+// Destructuring
+// ‘Enhanced’ object assignment (same key/value shortcut)
+
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre']);
+
+/*
+  {
+    first: "Tom",
+    second: "Margaret",
+    third: "Allison",
+    rest: ["David", "Pierre"]
+  }
+*/
+
 // --------------------------------------------------------------------
 
 // --------------------------------------------------------------------
