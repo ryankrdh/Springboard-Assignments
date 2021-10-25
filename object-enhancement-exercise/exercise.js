866d0758dbd104dc5c01c06858b8a0d376d3a8eb
Ryan Kim <ryankrdh@gmail.com>
2021-10-24 20:47:53 -0700

    working on object enhancement exercise

--- object-enhancement-exercise/exercise.js
+++ object-enhancement-exercise/exercise.js
@@ -60,3 +60,28 @@ const instructor = {
     return this.firstName + ' says bye!';
   },
 };
+
+// 4. createAnimal function
+// Write a function which generates an animal object. The function should accepts 3 arguments:
+
+// species: the species of animal (‘cat’, ‘dog’)
+// verb: a string used to name a function (‘bark’, ‘bleet’)
+// noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+// Use one or more of the object enhancements we’ve covered.
+
+const d = createAnimal('dog', 'bark', 'Woooof!');
+// {species: "dog", bark: ƒ}
+d.bark(); //"Woooof!"
+
+const s = createAnimal('sheep', 'bleet', 'BAAAAaaaa');
+// {species: "sheep", bleet: ƒ}
+s.bleet(); //"BAAAAaaaa"
+
+function createAnimal(species, verb, noise) {
+  return {
+    species,
+    [verb]() {
+      return noise;
+    },
+  };
+}
