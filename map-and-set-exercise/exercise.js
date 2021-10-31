3b8ed45a066156cded0de25a9b4c206fd6b9bd23
Ryan Kim <ryankrdh@gmail.com>
2021-10-31 13:48:42 -0700

    map and set exercise

--- map-and-set-exercise/exercise.js
+++ map-and-set-exercise/exercise.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// -------------
+// Maps and Sets Exercise
+// -----------------------------------------------
+// Quick Question #1
+// What does the following code return?
+
+new Set([1, 1, 2, 2, 3, 4]); // {1, 2, 3, 4}
+
+// -----------------------------------------------
+// Quick Question #2
+// What does the following code return?
+
+[...new Set('referee')].join(''); // "ref"
+// -----------------------------------------------
+// Quick Questions #3
+// What does the Map m look like after running the following code?
+
+let m = new Map();
+m.set([1, 2, 3], true);
+m.set([1, 2, 3], false);
+
+// {[1, 2, 3] => true, [1, 2, 3] => false}
+// -----------------------------------------------
+// hasDuplicate
+// Write a function called hasDuplicate which accepts an array and returns true or false if that array contains a duplicate
+
+hasDuplicate([1, 3, 2, 1]); //
+hasDuplicate([1, 5, -1, 4]); //
+
+// -----------------------------------------------
+
+// -----------------------------------------------
+
+// -----------------------------------------------
+
+// -----------------------------------------------
+
+// -----------------------------------------------
+
+// -----------------------------------------------
+
+// -----------------------------------------------
