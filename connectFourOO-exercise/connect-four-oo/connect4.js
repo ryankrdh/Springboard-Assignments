8f54f5e602c9b1b239e97efd10da748b13e8f2e9
ryankrdh <ryankrdh@gmail.com>
2021-11-16 13:56:07 -0800

    starting OO connect four exercise

--- connectFourOO-exercise/connect-four-oo/connect4.js
+++ connectFourOO-exercise/connect-four-oo/connect4.js
@@ -1,3 +1,45 @@
+// **** EXERCISE *****
+
+// Part One: Make Game Into a Class
+// Right now, our Connect Four is a bunch of disconnected functions and a few global variables.
+
+// This can make it hard to see how things work, and would make it hard to restart a game (quick—which variables would you have to reset to start a game?)
+
+// Let’s move this to being a class.
+
+// Initially, we’ll start with one class, Game. The players will still just be numbers for player #1 and #2.
+
+// What are the instance variables you’ll need on the Game?
+// for example: height, width, and the board will move from global variables to instance attributes on the class. What else should move?
+// Make a constructor that sets default values for these
+// Move the current functions onto the class as methods
+// This will require mildly rewriting some of these to change how you access variables and call other methods
+// You should end up with all of the code being in the Game class, with the only other code being a single line at the bottom:
+
+// new Game(6, 7);   // assuming constructor takes height, width
+// Part Two: Small Improvements
+// Make it so that you have a button to “start the game” — it should only start the game when this is clicked, and you should be able to click this to restart a new game.
+
+// Add a property for when the game is over, and make it so that you can’t continue to make moves after the game has ended.
+
+// Part Three: Make Player a Class
+// Right now, the players are just numbers, and we have hard-coded player numbers and colors in the CSS.
+
+// Make it so that there is a Player class. It should have a constructor that takes a string color name (eg, “orange” or “#ff3366”) and store that on the player instance.
+
+// The Game should keep track of the current player object, not the current player number.
+
+// Update the code so that the player pieces are the right color for them, rather than being hardcoded in CSS as red or blue.
+
+// Add a small form to the HTML that lets you enter the colors for the players, so that when you start a new game, it uses these player colors.
+
+// Further Study
+// If you have more time and would like more tasks, here are some things to play with:
+
+// Make it so that you can have more than two players
+// The look-and-feel is very sparse: add animations, better graphics for the board or pieces, and other CSS ideas. You could even use bootstrap for things like modals for the start-new-game form.
+// Make a very simple computer player: it could pick a random column and place a piece there. Can you do this in an object-oriented way, so there is a ComputerPlayer class?
+
 /** Connect Four
  *
  * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
@@ -5,11 +47,31 @@
  * board fills (tie)
  */
 
-const WIDTH = 7;
-const HEIGHT = 6;
+// const WIDTH = 7;
+// const HEIGHT = 6;
 
-let currPlayer = 1; // active player: 1 or 2
+// let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
+
+class Game {
+  constructor(player1, player2, HEIGHT = 6, WIDTH = 7) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.HEIGHT = HEIGHT;
+    this.WIDTH = WIDTH;
+    this.currPlayer = 1;
+    this.makeBoard();
+    this.makeHtmlBoard();
+    this.gameOver = false;
+  }
+
+  makeBoard() {
+    this.board = [];
+    for (let y = 0; y < this.HEIGHT; y++) {
+      this.board.push(Array.from({ length: this.WIDTH }));
+    }
+  }
+}
 
 /** makeBoard: create in-JS board structure:
  *   board = array of rows, each row is array of cells  (board[y][x])
@@ -97,17 +159,18 @@ function handleClick(evt) {
   // place piece in board and add to HTML table
   board[y][x] = currPlayer;
   placeInTable(y, x);
-  
+
   // check for win
-  if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
+  if (this.checkForWin()) {
+    this.gameOver = true;
+    return this.endGame(`Player ${this.currPlayer} won!`);
   }
-  
+
   // check for tie
-  if (board.every(row => row.every(cell => cell))) {
+  if (board.every((row) => row.every((cell) => cell))) {
     return endGame('Tie!');
   }
-    
+
   // switch players
   currPlayer = currPlayer === 1 ? 2 : 1;
 }
@@ -134,10 +197,30 @@ function checkForWin() {
     for (let x = 0; x < WIDTH; x++) {
       // get "check list" of 4 cells (starting here) for each of the different
       // ways to win
-      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+      const horiz = [
+        [y, x],
+        [y, x + 1],
+        [y, x + 2],
+        [y, x + 3],
+      ];
+      const vert = [
+        [y, x],
+        [y + 1, x],
+        [y + 2, x],
+        [y + 3, x],
+      ];
+      const diagDR = [
+        [y, x],
+        [y + 1, x + 1],
+        [y + 2, x + 2],
+        [y + 3, x + 3],
+      ];
+      const diagDL = [
+        [y, x],
+        [y + 1, x - 1],
+        [y + 2, x - 2],
+        [y + 3, x - 3],
+      ];
 
       // find winner (only checking each win-possibility as needed)
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
@@ -147,5 +230,9 @@ function checkForWin() {
   }
 }
 
-makeBoard();
-makeHtmlBoard();
+document.querySelector('#startButton').addEventListener('click', () => {
+  console.log('hello');
+});
+
+// makeBoard();
+// makeHtmlBoard();
