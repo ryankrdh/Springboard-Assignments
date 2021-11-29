4322c93922ec828300975fd13c82a2133a29f327
ryankrdh <ryankrdh@gmail.com>
2021-11-28 21:20:57 -0800

    working on part one

--- AJAX exercise/app.js
+++ AJAX exercise/app.js
@@ -10,3 +10,23 @@
 // We’ve provided a starter HTML file, but there’s next to nothing in the body. Start by building a simple form with an input for a search term and a submit button. When the user submits the form, use axios to make a request to GIPHY for information based on that term. After receiving the response, console.log the response data to make sure you’re getting back what you expect.
 
 // For example, here is what the AJAX request URL would look like for search term of “hilarious”: http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym. You can click on this URL and see the JSON you will get back. To view this in a nicer format, we highly recommend using the JSON Viewer chrome extension. This is also the data you should console.log when the form is submitted.
+
+// API WEBSITE: https://developers.giphy.com/docs/api/endpoint#search
+
+// QUESTIONS: How to find the API KEY?
+// QUESTION: evt.preventDefault(). I understand that this prevents the default action of such as checkbox or submit.. but why do we use it? and is it a good practice to use it on every jquery on method.
+
+// const $appendGif = $('#append-gif');
+const $userSearch = $('#user-search');
+
+async function getData(searchTerm) {
+  const res = await axios.get('http://api.giphy.com/v1/gifs/search', {
+    param: {
+      q: searchTerm,
+      api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym',
+    },
+  });
+  console.log(res);
+}
+
+getData('dog');
