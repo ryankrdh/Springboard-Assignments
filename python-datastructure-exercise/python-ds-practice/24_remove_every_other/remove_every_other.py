f7f9dec1af9d2ab7fcb40d0603ffce89b18bdc93
ryankrdh <ryankrdh@gmail.com>
2021-12-21 10:52:03 -0800

    python datastructure exercise

--- python-datastructure-exercise/python-ds-practice/24_remove_every_other/remove_every_other.py
+++ python-datastructure-exercise/python-ds-practice/24_remove_every_other/remove_every_other.py
@@ -11,3 +11,12 @@ def remove_every_other(lst):
         >>> lst
         [1, 2, 3, 4, 5]
     """
+    # return [lst[i] for i in range(len(lst)) if i%2 == 0]
+
+    # other solution:
+    return lst[::2]
+    
+    
+# lst = [1, 2, 3, 4, 5]
+
+# print(remove_every_other(lst))
