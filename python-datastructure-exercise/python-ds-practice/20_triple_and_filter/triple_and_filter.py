6900155b82403d787afbd1f6a288fddf6be30065
ryankrdh <ryankrdh@gmail.com>
2021-12-20 23:28:24 -0800

    python datastructure exercise

--- python-datastructure-exercise/python-ds-practice/20_triple_and_filter/triple_and_filter.py
+++ python-datastructure-exercise/python-ds-practice/20_triple_and_filter/triple_and_filter.py
@@ -13,3 +13,7 @@ def triple_and_filter(nums):
         >>> triple_and_filter([1, 2])
         []
     """
+    return [num * 3 for num in nums if num % 4 == 0]
+
+
+# print(triple_and_filter([1, 2, 3, 4, 5]))
