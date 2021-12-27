450ebbd4bd8bb51452489045d3939c9a81f4b8eb
ryankrdh <ryankrdh@gmail.com>
2021-12-26 17:45:26 -0800

    python data structure

--- python-datastructure-exercise/python-ds-practice/fs_4_reverse_vowels/reverse_vowels.py
+++ python-datastructure-exercise/python-ds-practice/fs_4_reverse_vowels/reverse_vowels.py
@@ -19,3 +19,27 @@ def reverse_vowels(s):
     reverse_vowels("why try, shy fly?")
     'why try, shy fly?''
     """
+    # Even though set can hold only unique items, it is significantly faster than lists when you want to check if an item is contained within. list is slightly faster when you are iterating over the values.
+    vowels = set("aeiou")
+    # print(vowels)
+    string = list(s)
+    # print(string)
+    i = 0
+    j = len(s) -1
+
+    # this while loop will make sure I don't iterate over the vowels again.
+    while i < j:
+        if string[i].lower() not in vowels:
+            i += 1
+        elif string[j].lower() not in vowels:
+            j -= 1
+        # it will iterate and count up i and count down j until it hits a vowel
+        else:
+            string[i], string[j] = string[j], string[i]
+            i += 1
+            j -= 1
+
+    return ''.join(string)
+
+        
+print(reverse_vowels("Reverse Vowels In A String"))
