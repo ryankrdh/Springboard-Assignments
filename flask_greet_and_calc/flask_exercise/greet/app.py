a840f2b933cdf03e173143bf6d7414dc408af51e
ryankrdh <ryankrdh@gmail.com>
2022-01-10 14:59:41 -0800

    flask2 exercise

---
+++ flask_greet_and_calc/flask_exercise/greet/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+    return """
+    welcome
+    """
+
+@app.route('/welcome/home')
+def home():
+    return """
+    welcome home
+    """
+
+@app.route('/welcome/back')
+def back():
+    return """
+    welcome back
+    """
+
