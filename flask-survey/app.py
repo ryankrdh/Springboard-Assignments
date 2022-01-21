71932bc053c8db7409d2c9a1738dad29d8e36298
ryankrdh <ryankrdh@gmail.com>
2022-01-20 19:43:20 -0800

    question templates

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, request, render_template, redirect
+from os import supports_bytes_environ
+from flask import Flask, request, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
+
+# QUESTION: can I get an explanation to RESPONSES_KEY = "responses". why cant we just use responses = []
+# key names will use to store some things in the session;
+# put here as constants so we're guaranteed to be consistent in
+# our spelling of these
+RESPONSES_KEY = "responses"
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "rhk"
@@ -9,17 +17,48 @@ debug = DebugToolbarExtension(app)
 
 # all_question = [ item.question for item in satisfaction_survey.questions]
 
-responses = []
-
 @app.route("/")
 def home_page():
     """Shows the home page."""
 
     return render_template("survey_home.html", survey=satisfaction_survey)
 
+@app.route("/start", methods=["POST"])
+def start_survey():
+    """clears the session and starts the survey"""
+
+    # QUESTION: flask session is used to store temporary information. for permanent data, use a database. 
+    # So what's the difference between using an empty list vs session. Is it just for non sens. data like user logged in or not?
+    # QUESTION: When using get or post methods, do we need to use redirect?
+    session[RESPONSES_KEY] = []
+
+    return redirect("/questions/0")
+
 @app.route("/questions/0", methods=["POST"])
-def show_question():
+def show_question0():
+    """Displays the questions"""
+    all_question = [ item.question for item in satisfaction_survey.questions ]
+
+    
+
+
+    # QUESTION: difference between using render_template vs redirect?
+    return render_template("question0.html", question=all_question, survey=satisfaction_survey)
+
+@app.route("/questions/1", methods=["POST"])
+def show_question1():
     """Displays the questions"""
     all_question = [ item.question for item in satisfaction_survey.questions ]
     # QUESTION: difference between using render_template vs redirect?
-    return render_template("question0.html", question=all_question, survey=satisfaction_survey)
+    return render_template("question1.html", question=all_question, survey=satisfaction_survey)
+
+# QUESTION: why is it methods in python file and method in html?
+@app.route("/answer", methods=["POST"])
+def save_response():
+    """Saves responses and continues on to next question"""
+    all_question = [ item.question for item in satisfaction_survey.questions ]
+    # grabs the user response
+    choice = request.form["answer"]
+    responses.append(choice)
+    return redirect(f"/questions/1")
+    # if (len(responses) == len(satisfaction_survey.questions)):
