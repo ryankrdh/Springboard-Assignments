eab756714b21c11c630f7b5e1e1561009c5d6719
ryankrdh <ryankrdh@gmail.com>
2022-02-19 17:48:00 -0800

    notes

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -1,3 +1,22 @@
+from flask import Flask, request, render_template, session, jsonify
 from boggle import Boggle
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "rhk"
+
+boggle_game = Boggle()
+
+@app.route("/")
+def homepage():
+    """shows the game board"""
+
+    # saves the function make_board()n to board to use.
+    board = boggle_game.make_board()
+
+    # saves value of board to a key:board in session.
+    session['board'] = board
+
+    # 
+    highscore = session.get("highscore", 0)
