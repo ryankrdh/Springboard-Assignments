5381bb2c2a392a7469626f013476b79f009159b7
ryankrdh <ryankrdh@gmail.com>
2022-02-20 00:55:58 -0800

    working on js file

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -2,15 +2,14 @@ from flask import Flask, request, render_template, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
-boggle_game = Boggle()
 
 app = Flask(__name__)
 app.debug = True
 app.config["SECRET_KEY"] = "rhk"
 
-toolbar = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+toolbar = DebugToolbarExtension(app)
 
 @app.route("/")
 def homepage():
@@ -29,3 +28,28 @@ def homepage():
     nplays = session.get("nplays", 0)
 
     return render_template("index.html", board=board, highscore=highscore, nplays=nplays)
+
+
+@app.route("/check-word")
+def check_word():
+    """Check if word is in dictionary."""
+
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update nplays, update high score if appropriate."""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
--- flask-boggle/static/boggle.js
+++ flask-boggle/static/boggle.js
@@ -1,31 +1,96 @@
 class BoggleGame {
+  /* make a new game at this DOM id */
+
   constructor(boardId, secs = 60) {
-    this.secs = secs;
+    this.secs = secs; // game length
     this.showTimer();
 
     this.score = 0;
     this.words = new Set();
-    // QUESTION: I don't get why we are using jquery here. is it to connect this to the html?
     this.board = $('#' + boardId);
 
     // every 1000 msec, "tick"
     this.timer = setInterval(this.tick.bind(this), 1000);
 
-    // QUESTION: Why are we selecting this.board for this jquery?
     $('.add-word', this.board).on('submit', this.handleSubmit.bind(this));
   }
+
+  /* show word in list of words */
+
+  showWord(word) {
+    $('.words', this.board).append($('<li>', { text: word }));
+  }
+
+  /* show score in html */
+
+  showScore() {
+    $('.score', this.board).text(this.score);
+  }
+
+  /* show a status message */
+
+  showMessage(msg, cls) {
+    $('.msg', this.board).text(msg).removeClass().addClass(`msg ${cls}`);
+  }
+
+  /* handle submission of word: if unique and valid, score & show */
+
+  async handleSubmit(evt) {
+    evt.preventDefault();
+    const $word = $('.word', this.board);
+
+    let word = $word.val();
+    if (!word) return;
+
+    if (this.words.has(word)) {
+      this.showMessage(`Already found ${word}`, 'err');
+      return;
+    }
+
+    // check server for validity
+    const resp = await axios.get('/check-word', { params: { word: word } });
+    if (resp.data.result === 'not-word') {
+      this.showMessage(`${word} is not a valid English word`, 'err');
+    } else if (resp.data.result === 'not-on-board') {
+      this.showMessage(`${word} is not a valid word on this board`, 'err');
+    } else {
+      this.showWord(word);
+      this.score += word.length;
+      this.showScore();
+      this.words.add(word);
+      this.showMessage(`Added: ${word}`, 'ok');
+    }
+
+    $word.val('').focus();
+  }
+
+  /* Update timer in DOM */
 
   showTimer() {
     $('.timer', this.board).text(this.secs);
   }
 
-  async handleSubmit(evt) {
-    // QUESTION: I understand that preventDefault prevents the default actions of a website. But what does it actually do??
-    evt.preventDefault();
-    // QUESTION: does this mean select the word, if this.board exists?
-    const $word = $('.word', this.board);
+  /* Tick: handle a second passing in game */
 
-    let word = $word.val();
-    if (!word) return;
+  async tick() {
+    this.secs -= 1;
+    this.showTimer();
+
+    if (this.secs === 0) {
+      clearInterval(this.timer);
+      await this.scoreGame();
+    }
+  }
+
+  /* end of game: score and update message. */
+
+  async scoreGame() {
+    $('.add-word', this.board).hide();
+    const resp = await axios.post('/post-score', { score: this.score });
+    if (resp.data.brokeRecord) {
+      this.showMessage(`New record: ${this.score}`, 'ok');
+    } else {
+      this.showMessage(`Final score: ${this.score}`, 'ok');
+    }
   }
 }
