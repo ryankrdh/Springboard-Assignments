f1c45b108edcbc2fd92639bd792640497b22a2c2
ryankrdh <ryankrdh@gmail.com>
2022-03-23 21:53:42 -0700

    working on third query

--- sql_joins_exercise/queries.sql
+++ sql_joins_exercise/queries.sql
@@ -14,3 +14,9 @@ SELECT first_name, last_name, COUNT(*) AS total_vehicle FROM owners INNER JOIN v
 
 -- Third query:
 -- Count the number of cars for each owner and display the average price for each of the cars as integers. Display the owners first_name, last_name, average price and count of vehicles. The first_name should be ordered in descending order. Only display results with more than one vehicle and an average price greater than 10000. 
+
+SELECT first_name, last_name, ROUND(AVG(price)) as average_price, COUNT(*) AS vehicle_count
+FROM owners INNER JOIN vehicles on owners.id = vehicles.owner_id
+GROUP BY (first_name, last_name)
+HAVING COUNT(*) > 1 AND ROUND(AVG(price)) > 10000
+ORDER BY first_name DESC;
